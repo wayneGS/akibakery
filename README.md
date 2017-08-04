cc16240d51c015779de21466d834e130c236c9ad
wayneGS <wayneTCP@hotmail.com>
2017-08-03 23:05:24 -0400

    CodeCopied

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# akibakery
-Source code for AkiBakery Web
+# Aki Royalty
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.0.4.
+
+## Development server
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  appFullName: 'Aki Royalty Web',
+  appShortName: 'Aki Royalty',
+  version: 201707081211,
+  restBaseUrl: 'http://191.237.68.123',
+  authenticationUrl: 'http://191.237.68.123/akiroyalty'
+
+};
